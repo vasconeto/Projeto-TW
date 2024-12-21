0be568eb4c6107506a00fc2240de8e1ef790655f
DinisMiranda <40240120@esmad.ipp.pt>
2024-12-21 20:51:59 +0000

    divisions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.querySelector('.search-bar input');
-    const resultContainer = document.querySelector('.result-container');
-
-    const books = [
-        { title: 'The Way of Kings', author: 'Brandon Sanderson', cover: 'imgs/TheWayOfKings.png' },
-        { title: 'Words of Radiance', author: 'Brandon Sanderson', cover: 'imgs/WordsOfRadiance.png' },
-        { title: 'Oathbringer', author: 'Brandon Sanderson', cover: 'imgs/Oathbringer.png' },
-        
-    ];
-
-    function displayBooks(books) {
-        resultContainer.innerHTML = '';
-        books.forEach(book => {
-            const bookCard = document.createElement('div');
-            bookCard.classList.add('result-card');
-            bookCard.innerHTML = `
-                <img src="${book.cover}" alt="Capa do Livro" class="book-cover">
-                <div class="book-info">
-                    <h3>${book.title}</h3>
-                    <p>${book.author}</p>
-                </div>
-            `;
-            resultContainer.appendChild(bookCard);
+document.addEventListener('DOMContentLoaded', function() {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll('.slides img');
+    
+    function showSlides() {
+        slides.forEach((slide, index) => {
+            slide.style.opacity = '0';
         });
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.opacity = '1';
+        setTimeout(showSlides, 3000); // Change image every 3 seconds
     }
-
-    function filterBooks() {
-        const query = searchInput.value.toLowerCase();
-        const filteredBooks = books.filter(book => 
-            book.title.toLowerCase().includes(query) || 
-            book.author.toLowerCase().includes(query)
-        );
-        displayBooks(filteredBooks);
-    }
-
-    searchInput.addEventListener('input', filterBooks);
-
-    // Initial display of books
-    displayBooks(books);
+    
+    showSlides();
 });
